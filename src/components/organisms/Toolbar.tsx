d64252a5f2065116ce4bb0b77fa1b9a4c1342b42
Wajid2001 <67428865+Wajid2001@users.noreply.github.com>
2024-10-06 12:48:59 +0000

    fix UI

--- src/components/organisms/Toolbar.tsx
+++ src/components/organisms/Toolbar.tsx
@@ -4,7 +4,7 @@ import { CellFormatter } from "../molecules/Toolbar/CellFormatter";
 export const Toolbar = () => {
   return (
     <div className="p-2">
-      <div className="py-2 bg-white shadow-md rounded-xl border flex flex-row divide-x-2 justify-center">
+      <div className="py-2 bg-white shadow-md rounded-xl border flex flex-row divide-x-2 justify-center overflow-auto">
         <GridSizeEdit />
 
         <CellFormatter />
